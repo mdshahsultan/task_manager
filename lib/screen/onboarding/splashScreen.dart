3fe7fc99168a25c66079c45feb7c711844eae659
mdshahsultanrubel <mdshahsultanrubel@gmail.com>
2026-02-24 12:22:38 +0600

    add

--- lib/screen/onboarding/splashScreen.dart
+++ lib/screen/onboarding/splashScreen.dart
@@ -10,6 +10,10 @@ class splashScreen extends StatefulWidget {
 class _splashScreenState extends State<splashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text("Splash Screen"),
+      ),
+    );
   }
 }
