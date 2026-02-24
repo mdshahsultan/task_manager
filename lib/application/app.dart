557b686be0c732c55376bbd9ffd8ec85f9cb2628
mdshahsultanrubel <mdshahsultanrubel@gmail.com>
2026-02-24 11:44:02 +0600

    add

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/screen/onboarding/loginScreen.dart';
+import 'package:task_manager/screen/onboarding/pinVerificationScreen.dart';
+import 'package:task_manager/screen/onboarding/splashScreen.dart';
 import '../screen/home_screen.dart';
+import '../screen/onboarding/emailVerificationScreen.dart';
+import '../screen/onboarding/registrationScreen.dart';
+import '../screen/onboarding/setPasswordScreen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,7 +14,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      title: "Task Manager",
+      //home: HomeScreen(),
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => splashScreen(),
+        "/login" : (context) => loginScreen(),
+        "/registration" : (context) => registrationScreen(),
+        "/emailVerification" : (context) => emailVerificationScreen(),
+        "/pinVerification" : (context) => pinVerificationScreen(),
+        "/setPassword" : (context) => setPasswordScreen(),
+      },
     );
   }
 }
