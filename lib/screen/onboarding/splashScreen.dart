9a784a27fdc8b447463e079207a72210c640d73e
mdshahsultanrubel <mdshahsultanrubel@gmail.com>
2026-02-25 13:35:59 +0600

    add

--- lib/screen/onboarding/splashScreen.dart
+++ lib/screen/onboarding/splashScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_manager/style/style.dart';
 
 class splashScreen extends StatefulWidget {
   const splashScreen({super.key});
@@ -11,8 +13,15 @@ class _splashScreenState extends State<splashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Splash Screen"),
+      //Stack used for two components. First one main background and Second logo
+      body: Stack(
+        children: [
+          Screenackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: SvgPicture.asset("assets/images/splash_logo.svg", alignment: Alignment.center,),
+          )
+        ],
       ),
     );
   }
